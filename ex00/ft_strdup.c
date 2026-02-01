7e67fe3ba5027b11cfe7d50da3b53c6c94f12dcb
Edoardo Mastrantoni <emastran@e3r6p5.42roma.it>
2026-02-01 12:42:18 +0100

    agg

--- ex00/ft_strdup.c
+++ ex00/ft_strdup.c
@@ -3,44 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdell-or <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: emastran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/28 11:07:31 by mdell-or          #+#    #+#             */
-/*   Updated: 2026/01/28 14:48:45 by mdell-or         ###   ########.fr       */
+/*   Created: 2026/02/01 12:00:30 by emastran          #+#    #+#             */
+/*   Updated: 2026/02/01 12:27:12 by emastran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stdlib.h>
-//#include <unistd.h>
-
+#include <stdio.h>
 char	*ft_strdup(char *src)
 {
-	char	*str;
-	char	*p;
+	int		i;
 	int		len;
-
+	char	*dest;
+	
+	i = 0;
 	len = 0;
 	while (src[len])
 		len++;
-	str = (char *)malloc((len + 1) * sizeof(char));
-	p = str;
-	while (*src)
-		*p++ = *src++;
-	*p = '\0';
-	return (str);
-}
-/*
-int main(void)
-{
-	char	*s = "durdal";
-	char	*copy;
-
-	copy = ft_strdup(s);
-	if (copy)
+	dest = (char *)malloc((len + 1) * sizeof(char));
+	while (src[i] != '\0')
 	{
-		write(1, copy, 1);
-		write(1, "\n", 1);
+		dest[i] = src[i];
+		i++;
 	}
-	free (copy);
+	return(dest);
+
+}
+/*int main()
+{
+	char *bho = "durdal";
+	char *s;
+	
+	s = ft_strdup(bho);
+	printf("%s", s);
+	free (s);
 	return (0);
 }*/
