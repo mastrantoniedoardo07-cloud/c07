4bae924d068e3d346e68b58a06c663aa0cce9126
Edoardo Mastrantoni <emastran@e3r6p3.42roma.it>
2026-02-02 11:08:03 +0100

    agg

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -3,46 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdell-or <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: emastran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/28 14:32:26 by mdell-or          #+#    #+#             */
-/*   Updated: 2026/01/28 16:18:01 by mdell-or         ###   ########.fr       */
+/*   Created: 2026/02/02 09:52:59 by emastran          #+#    #+#             */
+/*   Updated: 2026/02/02 11:07:04 by emastran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-int	*ft_range( int min, int max)
+int *ft_range(int min, int max)
 {
-	int	*ret;
-	int	*temp;
+	int	*s;
+	int	i;
 
+	i = 0;
 	if (min >= max)
-		return (0);
-	ret = (int *)malloc(sizeof(int) * ((long long)max - min));
-	if (!ret)
-		return (0);
-	temp = ret;
+		return (NULL);
+	s = (int *)malloc((max - 1) * sizeof(int));
 	while (min < max)
-		*(temp++) = min++;
-	return (ret);
-}
-/*
-int	main(void)
-{
-	int	maximum = 16 ;
-	int	minimum = -3;
-	int	i = 0;
-	int	temp;
-
-	int *arr = ft_range(minimum, maximum);
-	while (i == 0)
 	{
-		temp = minimum - maximum;
-		i < temp;
+		s[i] = min;
+		i++;
+		min++;
+	}
+	return (s);	
+}
+/*int main()
+{
+	int *arr = ft_range(1, 10);
+	int size = 10 -1;
+	int i = 0;
+
+	while(i < size)
+	{
+		printf("%d ", arr[i]);
 		i++;
 	}
-	printf("%d\n", arr[i]);
-	return (0);
 }*/
